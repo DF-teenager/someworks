1c6c7befd2b47d08197c3780249b108c35c2f5dc
songhw <lhammering@gmail.com>
2017-12-22 19:35:07 +0800

    1222 yeal_sign page fe

--- year_sign/index.js
+++ year_sign/index.js
@@ -2,69 +2,45 @@ Vue.use(VAnimateCss.default);
 
 var vm = new Vue({
     el: '#app',
-    data() {
-        return {
-            page01: {
-                show: false,
-                transition1: {
-                    classes: 'slideInDown',
-                    duration: 1000,
-                    delay: 0,
-                },
-                transition2: {
-                    classes: 'zoomIn',
-                    duration: 1000,
-                    delay: 200
-                },
-                transition3: {
-                    classes: 'zoomIn',
-                    duration: 1000,
-                    delay: 400
-                },
-                transition4: {
-                    classes: 'fadeInUp',
-                    duration: 1000,
-                    delay: 600
-                },
-                transition5: {
-                    classes: 'zoomIn',
-                    duration: 1000,
-                    delay: 800
-                },
-                transition6: {
-                    classes: 'fadeInUp',
-                    duration: 1000,
-                    delay: 1000
-                },
-                transition7: {
-                    classes: 'zoomIn',
-                    duration: 1000,
-                    delay: 1200
-                }
-            },
-            page02: {
-                show: true,
-                // transition1: {
-                //     classes: 'slideInDown',
-                //     duration: 1000,
-                //     delay: 0,
-                // },
-                // transition2: {
-                //     classes: 'zoomIn',
-                //     duration: 1000,
-                //     delay: 200
-                // },
-                // transition3: {
-                //     classes: 'zoomIn',
-                //     duration: 1000,
-                //     delay: 400
-                // },
-                // transition4: {
-                //     classes: 'fadeInUp',
-                //     duration: 1000,
-                //     delay: 600
-                // }
-            }
+    data: {
+        transition1: {
+            classes: 'slideInDown',
+            duration: 1000,
+            delay: 0,
+        },
+        transition2: {
+            classes: 'zoomIn',
+            duration: 1000,
+            delay: 200
+        },
+        transition3: {
+            classes: 'zoomIn',
+            duration: 1000,
+            delay: 400
+        },
+        transition4: {
+            classes: 'fadeInUp',
+            duration: 1000,
+            delay: 600
+        },
+        transition5: {
+            classes: 'zoomIn',
+            duration: 1000,
+            delay: 800
+        },
+        transition6: {
+            classes: 'fadeInUp',
+            duration: 1000,
+            delay: 1000
+        },
+        page01: {
+            show: false,
+        },
+        page02: {
+            show: false,
+        },
+        page03: {
+            show: true,
         }
     }
 })
