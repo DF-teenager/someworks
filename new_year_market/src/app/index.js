a8d380dfc60b5dd22cad36c6a9b9b3092249bb4e
songhw <lhammering@gmail.com>
2018-01-25 18:43:55 +0800

    字体

--- new_year_market/src/app/index.js
+++ new_year_market/src/app/index.js
@@ -23,6 +23,7 @@ var home = Vue.extend({
             isDoorZoomOut: false,
             cantakeNum: 3,
             couponIds: [],
+            lottery_num: 0,
             marketCoupons: [
                 { id: 1, value: 2000, name: '现金卡', firstBit: '2', plusBit: '000', unit: '元', checked: false },
                 { id: 2, value: 50, name: '现金券', firstBit: '5', plusBit: '0', unit: '元', checked: false },
@@ -37,7 +38,13 @@ var home = Vue.extend({
                 { id: 11, value: '1%', name: '加息券', firstBit: '1', plusBit: '', unit: '%', checked: false },
                 { id: 12, value: 1500, name: '现金券', firstBit: '1', plusBit: '500', unit: '元', checked: false }
                 
-            ]
+            ],
+            openRulesDialog: false,
+            openBindDialog: false,
+            openRetakeDialog: false,
+            openTakedDialog: false,
+            openUntakeDialog: false,
+            openNotakeDialog: false
         }
     },
     computed: {
@@ -59,7 +66,7 @@ var home = Vue.extend({
     },
     mounted: function() {
         if(!this.show_progress) {
-            this.page_show = 1;
+            this.page_show = this.$route.meta.page;
         }
     },
     directives: {
@@ -92,17 +99,95 @@ var home = Vue.extend({
 
             self.marketCoupons.forEach(function(ele, idx, arr){
                 if(idx === index) {
-                    if(self.couponIds.length <= 2 || arr[index].checked) {
+                    if(self.lottery_num > 0 && self.couponIds.length < self.lottery_num || arr[index].checked) {
                         arr[idx].checked = !arr[idx].checked;
                         var pos = self.couponIds.indexOf(arr[idx].id);
                         pos === -1 ? self.couponIds.push(arr[idx].id) : self.couponIds.splice(pos, 1);
+                    } else if(self.lottery_num === 0) {
+                        // self.openNotakeDialog = true;
+                        self.page_show = 3;
                     } else {
-                        console.log('您可选的年货数量已用完如果想要重新选择直接点击不满意的年货即可～');
+                        self.openRetakeDialog = true;
                     }
                 }
             });
-            console.log(self.couponIds);
+            console.log(self.couponIds); 
+        },
+        closeBindDialog: function() {
+            this.openBindDialog = false;
+        },
+        closeRetakeDialog: function() {
+            this.openRetakeDialog = false;
+        },
+        closeTakedDialog: function() {
+            this.openTakedDialog = false;
+        },
+        closeUntakeDialog: function() {
+            this.openUntakeDialog = false;
+        },
+        closeNotakeDialog: function() {
+            this.openNotakeDialog = false;
+        },
+        closeRulesDialog: function() {
+            this.openRulesDialog = false;
+        },
+        globalCloseDialog: function() {
+            this.openRulesDialog = false;
+        },
+        // 免费拿走
+        takeWantCoupon: function() {
             
+            var self = this;
+            var data = {
+                act:'getnum',     
+            }
+
+            $.ajax({
+                type: 'POST',
+                url: '/webapi/action_supermarket',
+                data: data,
+                success: function(res) {
+                    if(res.code === 0) {
+                        self.lottery_num = res.data.lottery_num;
+                    } else {
+                        alert(res.message);
+                    }
+                },
+                error: function(error) {
+                    alert(error);
+                }
+            })
+        },
+        // 免费拿走
+        takeWantCoupon: function() {
+
+            var self = this;
+            var data = {
+                act:'take',
+                goods_id: self.couponIds.join(','),       
+            }
+            
+            if(self.couponIds.length > 0){
+                $.ajax({
+                    type: 'POST',
+                    url: '/webapi/action_supermarket',
+                    data: data,
+                    success: function(res) {
+                        if(res.code === 0) {
+                            self.openTakedDialog = true;
+                        } else {
+                            alert(res.message);
+                        }
+                    },
+                    error: function(error) {
+                        alert(error);
+                    }
+                })
+            } else if(self.lottery_num === 0) {
+                self.page_show = 3;
+            } else {
+                self.openUntakeDialog = true;
+            }
         }
     }
 })
@@ -110,14 +195,53 @@ var records = Vue.extend({
     template: '#records',
     data: function() {
         return {
-
+            coupons: []
         }
+    },
+    created: function() {
+        this.getRecords();
+    },
+    methods: {
+        /**
+         * 获取年货记录
+         */
+        getRecords: function() {
+            
+            var self = this;
+            var data = {
+                act:'list',
+            }
+
+            $.ajax({
+                type: 'POST',
+                url: '/webapi/action_supermarket',
+                data: data,
+                success: function(res) {
+                    if(res.code === 0) {
+                        self.coupons = res.data;
+                    } else {
+                        alert(res.message);
+                    }
+                },
+                error: function(error) {
+                    alert(error);
+                }
+            })
+        },
+    },
+    beforeRouteLeave (to, from, next) {
+        to.meta.page = from.params.page;
+        next();
     }
 })
 Vue.component('v-dialog', {
     template: '#dialog',
     props: {
         showDialog: {
+            type: Boolean,
+            default: false
+        },
+        isBtn: {
             type: Boolean,
             default: false
         }
@@ -128,15 +252,25 @@ Vue.component('v-dialog', {
         }
     },
     methods: {
-        cancelDialog: function() {
-
+        closeDialog: function() {
+            this.$emit('close-dialog');
+        },
+        globalCloseDialog: function() {
+            this.$emit('global-close-dialog');
         }
     }
 })
 var routes = [{
-        name: 'home', path: '/', component: home
+        name: 'home', 
+        path: '/', 
+        component: home,
+        meta: {
+            page: 1
+        }
     },{   
-        name: 'records', path: '/records', component: records
+        name: 'records', 
+        path: '/records/:page', 
+        component: records
     }]
 var router = new Router({
     routes: routes
@@ -149,7 +283,22 @@ new Vue({
             show_progress: true,
             progress_percent: 0,
             imgs: [
-                'https://caiyunupload.b0.upaiyun.com/ydimg/theme/2017/12/year_sign/progress.jpg',
+                'https://caiyunupload.b0.upaiyun.com/ydimg/theme/2018/01/new_year_market/snowing-bg.png',
+                'https://caiyunupload.b0.upaiyun.com/ydimg/theme/2018/01/new_year_market/page-1-1s.jpg',
+                'https://caiyunupload.b0.upaiyun.com/ydimg/theme/2018/01/new_year_market/gomarket-sprites_min.png',
+                'https://caiyunupload.b0.upaiyun.com/ydimg/theme/2018/01/new_year_market/page-1-2.png',
+                'https://caiyunupload.b0.upaiyun.com/ydimg/theme/2018/01/new_year_market/page-2-1ss.jpg',
+                'https://caiyunupload.b0.upaiyun.com/ydimg/theme/2018/01/new_year_market/page-2-2s.png',
+                'https://caiyunupload.b0.upaiyun.com/ydimg/theme/2018/01/new_year_market/page-2-3.png',
+                'https://caiyunupload.b0.upaiyun.com/ydimg/theme/2018/01/new_year_market/page-2-4.png',
+                'https://caiyunupload.b0.upaiyun.com/ydimg/theme/2018/01/new_year_market/page-2-4-mask.png',
+                'https://caiyunupload.b0.upaiyun.com/ydimg/theme/2018/01/new_year_market/page-2-3-mask.png',
+                'https://caiyunupload.b0.upaiyun.com/ydimg/theme/2018/01/new_year_market/page-3-1s.jpg',
+                'https://caiyunupload.b0.upaiyun.com/ydimg/theme/2018/01/new_year_market/page-3-3.png',
+                'https://caiyunupload.b0.upaiyun.com/ydimg/theme/2018/01/new_year_market/page-3-4.png',
+                'https://caiyunupload.b0.upaiyun.com/ydimg/theme/2018/01/new_year_market/page-4-1s.jpg',
+                'https://caiyunupload.b0.upaiyun.com/ydimg/theme/2018/01/new_year_market/dialog-1.png',
+                'https://caiyunupload.b0.upaiyun.com/ydimg/theme/2018/01/new_year_market/dialog-2.png'
             ]
         }
     },
